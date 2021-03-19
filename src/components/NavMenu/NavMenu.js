51280fda4806e33253495915bcdd9f101c7ebb12
Andran1 <Andran1@users.noreply.github.com>
2021-03-20 00:09:56 +0400

    add logika login end register peges

--- src/components/NavMenu/NavMenu.js
+++ src/components/NavMenu/NavMenu.js
@@ -1,4 +1,4 @@
-import { Navbar, Nav, Container, Row,Col } from "react-bootstrap";
+import { Navbar, Nav, Container, Row, Col } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 import styles from "./navMenu.module.css";
 
@@ -7,7 +7,7 @@ export default function NavMenu() {
     <Container>
       <Row>
         <Col>
-          <Navbar bg="dark" variant="dark">
+          <Navbar bg="dark" variant="dark" style={{display:"flex", justifyContent:"space-between",alignItems:"center"}}>
             <Nav className={styles.navBar}>
               <NavLink activeClassName={styles.active} exact to="/">
                 Home
@@ -19,6 +19,14 @@ export default function NavMenu() {
                 Contact
               </NavLink>
             </Nav>
+            <div style={{width:"100px", display:"flex", justifyContent:"space-between",alignItems:"center"}}>
+              <NavLink  to="/login">
+                Log In
+              </NavLink>
+              <NavLink  to="/register">
+                Sin In
+              </NavLink>
+            </div>
           </Navbar>
         </Col>
       </Row>
--- src/components/pages/Register/Register.js
+++ src/components/pages/Register/Register.js
@@ -1,8 +1,62 @@
-import React from "react";
-import { Row, Col, Form, Button, Container,Card } from "react-bootstrap";
+import React, { useState } from "react";
+import { Row, Col, Form, Button, Container, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 function Register() {
+  const [values, setValues] = useState({
+    name: "",
+    surname: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [errors, setErrors] = useState({
+    name: null,
+    surname: null,
+    email: null,
+    password: null,
+    confirmPassword: null,
+  });
+
+  const handleSubmit = (props) => {
+    const { name, surname, email, password, confirmPassword } = values;
+    let valid = true;
+    let passwordMessage = null;
+
+
+    if (!confirmPassword) {
+      passwordMessage = "Password  is required";
+      valid = false;
+    } else if (password !== confirmPassword) {
+      passwordMessage = "Password  didn't match";
+      valid = false;
+    }
+
+    setErrors({
+      name: name ? null : "name is required ",
+      surname:surname? null :"surname is required",
+      email: email ? null : "email is required",
+      password: password ? null : "password is required",
+      confirmPassword: passwordMessage
+    });
+
+    if(valid){
+       props.register(values)
+    }
+  };
+
+  const handleChange = ({ target: { name, value } }) => {
+   setValues({
+       ...values,
+       [name]:value
+   })
+   setErrors({
+       ...errors,
+       [name]:null
+   })
+  
+  };
+
   return (
     <div style={{ width: "400px", margin: " 0 auto" }}>
       <Container>
@@ -21,26 +75,62 @@ function Register() {
                 </span>
               </div>
               <Form.Group controlId="formBasicEmail">
-                <Form.Control type="email" placeholder="Enter your name" />
+                <Form.Control 
+                type="text" 
+                placeholder="Enter your name" 
+                name="name"
+                value={values.name}
+                onChange={handleChange}
+                />
               </Form.Group>
 
               <Form.Group controlId="formBasicPassword">
-                <Form.Control type="password" placeholder="Enter your Password" />
+                <Form.Control
+                  type="text"
+                  placeholder="Enter your Password"
+                  name="surname"
+                  value={values.surname}
+                  onChange={handleChange}
+                />
               </Form.Group>
               <Form.Group controlId="formBasicEmail">
-                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Control 
+                type="email"
+                 placeholder="Enter email" 
+                 name="email"
+                 value={values.email}
+                 onChange={handleChange}
+
+                 />
               </Form.Group>
 
               <Form.Group controlId="formBasicPassword">
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Control
+                 type="text"
+                 placeholder="Password" 
+                 name="password"
+                 value={values.password}
+                 onChange={handleChange}
+                 
+                 />
               </Form.Group>
               <Form.Group controlId="formBasicEmail">
-                <Form.Control type="email" placeholder="Confirm Password" />
-              </Form.Group>
+                <Form.Control 
+                type="email" 
+                placeholder=" confirm password" 
+                name="confirmPassword"
+                value={values.confirmPassword}
+                onChange={handleChange}
 
+                />
+              </Form.Group>
 
               <div style={{ display: "flex", justifyContent: "center" }}>
-                <Button variant="primary" type="submit">
+                <Button 
+                variant="primary"
+                 type="submit"
+                 onClick={handleSubmit}
+                 >
                   Register
                 </Button>
               </div>
